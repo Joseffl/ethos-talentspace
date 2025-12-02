e94b0bff841a296d8a25a4f947a7e5739001f31c
Joseffl <timmyjoseph505@gmail.com>
2025-12-02 12:36:10 +0100

    new

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,5 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
-import arcjet, { shield } from "@arcjet/next"
-import { env } from "./data/env/server"
-import { NextResponse } from "next/server"
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
   "/",
@@ -10,53 +8,28 @@ const isPublicRoute = createRouteMatcher([
   "/api(.*)",
   "/courses/:courseId/lessons/:lessonId",
   "/products(.*)",
-])
+]);
 
-const isAdminRoute = createRouteMatcher(["/admin(.*)"])
-
-const aj = arcjet({
-  key: env.ARCJET_KEY,
-  rules: [
-    shield({ mode: "LIVE" }),
-    
-  ],
-})
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  let decision
-  if (!isPublicRoute(req)) {
-    decision = await aj.protect(
-      env.TEST_IP_ADDRESS
-        ? { ...req, ip: env.TEST_IP_ADDRESS, headers: req.headers }
-        : req
-    )
-
-    if (decision.isDenied()) {
-      return new NextResponse(null, { status: 403 })
-    }
-  }
-
   if (isAdminRoute(req)) {
-    const user = await auth.protect()
+    const user = await auth.protect();
     if (user.sessionClaims.role !== "admin") {
-      return new NextResponse(null, { status: 404 })
+      return new NextResponse(null, { status: 404 });
     }
   }
 
   if (!isPublicRoute(req)) {
-    await auth.protect()
+    await auth.protect();
   }
 
-  if (decision && !decision.ip.isVpn() && !decision.ip.isProxy()) {
-    return NextResponse.next()
-  }
-})
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
-}
+};
