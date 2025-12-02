cab56564c188822a807591e6fd352c9e3023c05b
Joseffl <timmyjoseph505@gmail.com>
2025-12-02 12:26:42 +0100

    newrun

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
-import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/next"
+import arcjet, { shield } from "@arcjet/next"
 import { env } from "./data/env/server"
 import { NextResponse } from "next/server"
 
@@ -18,20 +18,11 @@ const aj = arcjet({
   key: env.ARCJET_KEY,
   rules: [
     shield({ mode: "LIVE" }),
-    detectBot({
-      mode: "LIVE",
-      allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:MONITOR", "CATEGORY:PREVIEW"],
-    }),
-    slidingWindow({
-      mode: "LIVE",
-      interval: "1m",
-      max: 100,
-    }),
+    
   ],
 })
 
 export default clerkMiddleware(async (auth, req) => {
-  // Only apply Arcjet protection to non-public routes
   let decision
   if (!isPublicRoute(req)) {
     decision = await aj.protect(
