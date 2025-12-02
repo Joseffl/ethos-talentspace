2343570a51c11cf3957e049a2f969df24058100e
Joseffl <timmyjoseph505@gmail.com>
2025-12-02 10:32:40 +0100

    deploy

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,22 @@
 import { env } from "@/data/env/server"
 import { defineConfig } from "drizzle-kit"
 
+// export default defineConfig({
+//     out: "./src/drizzle/migrations",
+//     schema: "./src/drizzle/schema.ts",
+//     strict: true,
+//     verbose: true,
+//     dialect: "postgresql",
+//     dbCredentials: {
+//         password: env.DB_PASSWORD,
+//         user: env.DB_USER,
+//         database: env.DB_NAME,
+//         host: env.DB_HOST,
+//         ssl: false,
+//     }
+    
+// })
+
 export default defineConfig({
     out: "./src/drizzle/migrations",
     schema: "./src/drizzle/schema.ts",
@@ -8,11 +24,6 @@ export default defineConfig({
     verbose: true,
     dialect: "postgresql",
     dbCredentials: {
-        password: env.DB_PASSWORD,
-        user: env.DB_USER,
-        database: env.DB_NAME,
-        host: env.DB_HOST,
-        ssl: false,
+        url: env.DATABASE_URL,
     }
-    
 })
--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -2,12 +2,16 @@ import { env } from "@/data/env/server"
 import { drizzle } from "drizzle-orm/node-postgres"
 import * as schema from "./schema"
 
+// export const db = drizzle({
+//   schema,
+//   connection: {
+//     password: env.DB_PASSWORD,
+//     user: env.DB_USER,
+//     database: env.DB_NAME,
+//     host: env.DB_HOST,
+//   },
+// })
 export const db = drizzle({
   schema,
-  connection: {
-    password: env.DB_PASSWORD,
-    user: env.DB_USER,
-    database: env.DB_NAME,
-    host: env.DB_HOST,
-  },
+  connection: env.DATABASE_URL,
 })
