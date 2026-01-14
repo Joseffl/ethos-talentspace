4b2985012e3eb8e0c15afab93e258222533323f3
Joseffl <timmyjoseph505@gmail.com>
2026-01-14 11:44:48 +0100

    workflow changed

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,24 @@
+// import { env } from "@/data/env/server"
+// import { defineConfig } from "drizzle-kit"
+
+
+
+// export default defineConfig({
+//     out: "./src/drizzle/migrations",
+//     schema: "./src/drizzle/schema.ts",
+//     strict: true,
+//     verbose: true,
+//     dialect: "postgresql",
+//     dbCredentials: {
+//         url: env.DATABASE_URL,
+//     }
+// })
+
+import { config } from "dotenv";
+config({ path: ".env.local" }); 
+
 import { env } from "@/data/env/server"
 import { defineConfig } from "drizzle-kit"
-
-
 
 export default defineConfig({
     out: "./src/drizzle/migrations",
@@ -10,6 +27,6 @@ export default defineConfig({
     verbose: true,
     dialect: "postgresql",
     dbCredentials: {
-        url: env.DATABASE_URL,
+        url: process.env.DATABASE_URL!, 
     }
 })
